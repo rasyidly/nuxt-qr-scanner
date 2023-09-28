da5f6db7a9f65367f65f71440ebf83b4a79e9689
rasyidly <rasyid@sekeco.id>
2023-09-28 07:22:15 +0700

    update table

---
+++ pages/table.vue
@@ -0,0 +1,16 @@
+<template>
+    <table>
+        <tbody>
+            <tr v-for="(registrant, i) of registrants">
+                <td>{{ i + 1 }}</td>
+                <td>{{ registrant.id }}</td>
+                <td>{{ registrant.name }}</td>
+                <td>{{ registrant.p }}</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script setup>
+import { registrants } from '~/db/satfest'
+</script>
